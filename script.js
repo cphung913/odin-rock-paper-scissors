23dd86fd53926648350e0841b8655cc6fa4bf7f4
cphung913 <chasephung13@gmail.com>
2023-06-25 16:02:42 -0700

    reimplemented logic to work with UI

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
 const options = [rockButton, paperButton, scissorsButton];
+const submitButton = document.querySelector(".submit");
+const message = document.querySelector(".message");
+const score = document.querySelector("#score");
+
 let playerScore = 0;
 let computerScore = 0;
+let playerChoice;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -27,22 +32,35 @@ function getComputerChoice() {
 }
 
 function game() {
-    for(let i=0; i < 5; i++) {
-        let playerChoice = playerSelection();
-        let computerChoice = getComputerChoice();
-
-        if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") {
-            playerScore++;
-            
-        } else if (playerChoice == computerChoice) {
-            
-        } else {
-            computerScore++;
-        }
+    if(playerChoice == undefined) {
+        message.textContent = "Please select Rock, Paper, or Scissors";
+        return;
     }
+
+    const computerChoice = getComputerChoice();
+    let state = "";
+
+    if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") {
+        playerScore++;
+        state = "You Win!";
+    } else if (playerChoice == computerChoice) {
+        state = "Tie!";
+    } else {
+        computerScore++;
+        state = "I Win!";
+    }
+
+    message.textContent = "You chose " + playerChoice + ". I chose " + computerChoice + ". " + state;
+    score.textContent = playerScore + " - " + computerScore;
+
+    playerChoice = undefined;
+    options.forEach(i => i.classList.remove("selected"));
 }
 
 options.forEach(button =>  button.addEventListener('click', function(e) {
     options.forEach(i => i.classList.remove("selected"));
+    playerChoice = `${button.className}`;
     button.classList.add("selected");
 }))
+
+submitButton.addEventListener('click', game);
